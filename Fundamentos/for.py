aea36cc1fa23d15e7efad80a999995af81e5b4b7
carlosouzavalle <carlosouzavalle@gmail.com>
2024-08-17 20:42:45 -0300

    for

---
+++ Fundamentos/for.py
@@ -0,0 +1,15 @@
+# laço de repetição for
+
+num = 0
+
+for valores in range(10): # Loop de 0 a 9, totalizando 10 iterações
+    num += 1 
+    print(valores)
+
+# o for funciona diferente de outras linguagens
+
+print('================================================================')
+
+nome = 'Carlos'
+for letras in nome:
+    print(letras)
--- Fundamentos/while.py
+++ Fundamentos/while.py
@@ -1,9 +1,21 @@
 # laço de repetição
+# vai repetir até que uma determinada confição seja atendida e ele finalize a execução
 
-num = 1
+# num = 1
+
+# while(num <= 10):
+#     print(num)
+#     num += 1
+# print('fim do laço')
 
 
-while(num <= 10):
-    print(num)
-    num += 1
-print('fim do laço')
+
+nome = None
+
+while True:
+    print('Digite seu nome ou X para parar: ', end='')
+    nome = input()
+    if nome == 'x' or nome == 'X':
+        break
+    print(f'Olá bem-vindo, {nome}')
+print('Até logo!')
