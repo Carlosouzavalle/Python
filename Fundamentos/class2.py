f256fc5fea26e7292b10c5a95a0f4c31d936ecb9
carlosouzavalle <carlosouzavalle@gmail.com>
2024-08-26 21:02:02 -0300

    class

---
+++ Fundamentos/class2.py
@@ -0,0 +1,11 @@
+from classs import Vendedor
+
+
+Vendedor1 = Vendedor('Carlos')
+Vendedor1.vendeu(1000)
+Vendedor1.bateu_meta(600)
+print(Vendedor1.nome)
+
+Vendedor2 = Vendedor('Bia')
+Vendedor2.vendeu(500)
+Vendedor2.bateu_meta(600)
