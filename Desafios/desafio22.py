2b95539538ffec03fb055887080799030de61ddf
carlosouzavalle <carlosouzavalle@gmail.com>
2024-10-23 23:39:28 -0300

    desafio22

---
+++ Desafios/desafio22.py
@@ -0,0 +1,9 @@
+nome = input('Digite seu nome: ')
+novoNome = nome.upper()
+novoNome2 = nome.lower()
+novoNome3 = len(nome)
+
+print(f'Seu nome em maiuscula fica: {novoNome}')
+print(f'Seu nome em minuscula fica: {novoNome2}')
+print(f'Seu nome tem {novoNome3} letras')
+print(f'Seu nome é {nome} e tem tantas {novoNome3} letras ')
