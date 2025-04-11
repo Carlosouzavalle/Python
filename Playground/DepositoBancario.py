727678e6324a14f006ec925cb4b7d5f45d4063f7
Carlosouzavalle <carlosouzavalle@gmail.com>
2025-04-11 09:57:13 -0300

    Novas funções

--- Playground/DepositoBancario.py
+++ Playground/DepositoBancario.py
@@ -1,32 +1,67 @@
 ContaCorrente = 0
 qtd_saques = 3
+qtd_deposito = 3
 ValorDeSaque = 0
+
      
 
-while True:
-        mensagemBoasVindas = input("Escolha uma opção 1: Deposito e 2: Saque ")
-
-        if mensagemBoasVindas == "1":
-            
-            ValorDeDeposito = float(input("Qual valor você quer depositar? "))
-            if ValorDeDeposito <= 0:
-                print("ERRO!!! O Valor informado não é valido.")
+                                 
+def deposito():
+        global ContaCorrente, qtd_deposito 
+        
+        if qtd_deposito == 0:
+            print("Limite de transações excedido!!!")
+            return
+        
+        ValorDeDeposito = float(input("Qual valor você quer depositar? "))
+        if ValorDeDeposito <= 0:
+            print("ERRO!!! O Valor informado não é valido.")
+        else:
+            ContaCorrente += ValorDeDeposito
+            qtd_deposito -=1
+            print(f"Seu saldo é de {ContaCorrente}R$ ")
+            print(f"Depósitos restantes hoje: {qtd_deposito}")
+        Menu()
+        
+        
+        
+        
+              
+def saque():
+    for s in range(5):
+        if qtd_saques == 0:
+            print("por hoje deu pai volta amanha")
+        else:
+            ValorDeSaque = float(input("qual valor você quer sacar? "))
+            if ValorDeSaque <= 0:
+                print("ERROR!!! valor invalido")
+            elif ValorDeSaque > ContaCorrente:
+                print("ERRO!!! Saldo insuficiente.")
             else:
-                ContaCorrente += ValorDeDeposito
-                print(f"Seu saldo é de {ContaCorrente}R$ ")
+                ContaCorrente -= ValorDeSaque
+                qtd_saques -=1
+                print(ContaCorrente)
+        Menu()
+                
+                
+def Abrir_conta():
+    NAME = input("Qual seu nome? ")
+    AGE = int(input("Quantos anos  você tem? "))
+    print("Conta Criada com sucesso")
+    print(f"Seja Bem-Vindo {NAME}")
+                
+                
+def Menu():
+    mensagemBoasVindas = input("Escolha uma opção 1: Deposito 2: Saque 3: Abrir Conta")                     
 
-       
-        elif mensagemBoasVindas == "2":  
-                if qtd_saques == 0:
-                    print("por hoje deu pai volta amanha")
-                else:
-                    ValorDeSaque = float(input("qual valor você quer sacar? "))
-                    if ValorDeSaque <= 0:
-                        print("ERROR!!! valor invalido")
-                    elif ValorDeSaque > ContaCorrente:
-                        print("ERRO!!! Saldo insuficiente.")
-                    else:
-                        ContaCorrente -= ValorDeSaque
-                        qtd_saques -=1
-                        print(ContaCorrente)
-                    
+    if mensagemBoasVindas == "1":
+        deposito()
+    elif mensagemBoasVindas == "2": 
+        saque()
+    elif mensagemBoasVindas == "3":
+        Abrir_conta()
+    else:
+        print("Opção invalida")
+
+
+Menu()
