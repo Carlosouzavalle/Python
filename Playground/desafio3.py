502e4f283f558db862baa7232990a15cfbd085dc
Carlosouzavalle <carlosouzavalle@gmail.com>
2025-05-01 21:36:22 -0300

    poo

--- Playground/desafio3.py
+++ Playground/desafio3.py
@@ -13,10 +13,16 @@ class loja():
         
     def correr(self, correr):
         self.correr = correr
+        
+    # def __str__(self):
+    #     return f"Bicicleta: Cor={self.cor} Modelo={self.modelo} Ano={self.ano} Valor={self.valor}"
 
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
 Bike1 = loja("Vermelha", "Caloi", 2022, 1200.00)
-print(Bike1.cor)
+print(Bike1)
 
 
 Bike1.buzinar("biiiii")   
